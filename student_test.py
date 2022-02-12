bf4bb74cd87ba596db29f9a9a7886bd4f868d495
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-12 18:34:48 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -32,3 +32,25 @@ def test_inputs():
     out,err = capsys.readouterr()
     assert out == "Hello world!\n" or "bye" in out, "Should read 'Hello world!' "
 """
+
+# Tests multiple values one at a time:
+
+# def test_higher():
+#     for i in [19, 91, 20, 24, 54, 33, 18]:
+#         input_values=[str(i)]
+#         output=[]
+
+#         def mock_input(s=None):
+#             if s is not None:
+#                 output.append(s)
+#                 return input_values.pop(0)
+#             else:
+#                 output.append("")
+#                 return input_values.pop(0)
+        
+#         student.input = mock_input
+#         student.print = lambda s : output.append(s)
+
+#         student.main()
+
+#         assert "true" in output[1].lower()
